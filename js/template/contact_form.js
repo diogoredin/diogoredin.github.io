67ea6c0876eca616a1232d57aca3621624e3024e
Diogo Redin <diogo.redin@gmail.com>
2017-10-09 18:24:06 +0100

    Testing with new form

--- js/template/contact_form.js
+++ js/template/contact_form.js
@@ -5,31 +5,48 @@
 *
 */
 
-$(document).ready(function() {
+$(document).ready(function () {
 
 	// Enable User Strict Mode
 	'use strict';
 
 	// Form is submitted
-	$('.btn-default').on("click", function(){
+	$('#contact_form').on("submit", function () {
 
 		// Store name, email and message
 		var name = $("#name").val();
 		var email = $("#email").val();
 		var message = $("#message").val();
-		
+
 		// Cleans previous message
 		$("#returnmessage").empty();
-	
+
 		// Checks for blank fields
 		if (name == '' || email == '') {
 			alert("Please fill the required fields.");
 		} else {
-			$("#returnmessage").append("<div class='contact_message'>Your message was successfully sent!</div>");
+
+			$.ajax({
+				url: "https://formspree.io/diogoredin@gmail.com",
+				method: "POST",
+				data: { message: message },
+				dataType: "json"
+			}, function (data) {
+
+				// Display returned message
+				$("#returnmessage").append(data);
+
+				// Reset the fields if the message was successfuly sent
+				if (data == "<div class='contact_message'>Your message was successfully sent!</div>") {
+					$("#contact_form")[0].reset();
+				}
+
+			});
+
 		}
 
-	// Ends Submission
-	return false;
+		// Ends Submission
+		return false;
 
 	});
 
