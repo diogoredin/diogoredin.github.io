e9f5e8764794340cd8a3a056730804dc61804b0f
Diogo Redin <diogo.redin@gmail.com>
2019-06-15 19:16:51 +0100

    html base for the studio page

--- js/main.js
+++ js/main.js
@@ -27,14 +27,45 @@ $(document).ready(function() {
         prevText: '<span class="icon is-medium"><span class="fa-stack"><i class="fas fa-circle fa-stack-2x"></i><i class="fas fa-angle-left fa-stack-1x fa-inverse"></i></span></span>'
     });
 
+    $(".header-studio").hide();
+    $(".table").hide();
+    $(".how").hide();
+    $(".contact").hide();
+
     $("#studio").on("click", function() {
         $("#personal").removeClass("is-info is-selected");
         $(this).addClass("is-info is-selected");
+
+        /* Hide */
+        $(".header-personal").hide();
+        $(".portfolio").hide();
+        $(".about").hide();
+        $(".pictures").hide();
+
+        /* Show */
+        $(".header-studio").show();
+        $(".table").show();
+        $(".how").show();
+        $(".contact").show();
+
     });
 
     $("#personal").on("click", function() {
         $("#studio").removeClass("is-info is-selected");
         $(this).addClass("is-info is-selected");
+
+        /* Hide */
+        $(".header-studio").hide();
+        $(".table").hide();
+        $(".how").hide();
+        $(".contact").hide();
+
+        /* Show */
+        $(".header-personal").show();
+        $(".portfolio").show();
+        $(".about").show();
+        $(".pictures").show();
+
     });
 
 });
